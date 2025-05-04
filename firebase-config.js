59ea03672393a44b2672a1a4e26ae61c263eb0e8
MiloMih <miloslav.mihucky@gmail.com>
2025-05-04 19:25:17 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyDNWRlWmBtQPIR49KViBpWf16hDUjU7Npw",
+  authDomain: "turnaj-skusobne.firebaseapp.com",
+  projectId: "turnaj-skusobne",
+  storageBucket: "turnaj-skusobne.firebasestorage.app",
+  messagingSenderId: "755998492681",
+  appId: "1:755998492681:web:f3242ae0f54b61903290d8",
+  measurementId: "G-B8DVMX2KVV"
+};
+
+// Inicializácia Firebase
+firebase.initializeApp(firebaseConfig);
