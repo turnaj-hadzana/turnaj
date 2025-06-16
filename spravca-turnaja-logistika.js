019c50b489e00cde6e40a0f78f9f4c69b72005b7
turnaj <miloslav.mihucky@gmail.com>
2025-06-16 09:29:18 +0200

    Update spravca-turnaja-logistika.js

--- spravca-turnaja-logistika.js
+++ spravca-turnaja-logistika.js
@@ -1010,7 +1010,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     const placeNameInput = document.getElementById('placeName');
     const placeAddressInput = document.getElementById('placeAddress');
     const placeGoogleMapsUrlInput = document.getElementById('placeGoogleMapsUrl');
-    const deletePlaceButtonModal = document = document.getElementById('deletePlaceButtonModal');
+    const deletePlaceButtonModal = document.getElementById('deletePlaceButtonModal');
 
     if (categoriesContentSection) {
         categoriesContentSection.style.display = 'block';
