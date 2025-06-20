7737dc6ca4d4a3b3db90b3919cb40f35b7828250
turnaj <miloslav.mihucky@gmail.com>
2025-06-20 19:40:33 +0200

    Update spravca-turnaja-zapasy.js

--- spravca-turnaja-zapasy.js
+++ spravca-turnaja-zapasy.js
@@ -1401,7 +1401,7 @@ async function displayMatchesAsSchedule() {
                                 const blockedSlotStartHour = String(Math.floor(blockedSlot.startInMinutes / 60)).padStart(2, '0');
                                 const blockedSlotStartMinute = String(blockedSlot.startInMinutes % 60).padStart(2, '0');
                                 const blockedSlotEndHour = String(Math.floor(blockedSlot.endInMinutes / 60)).padStart(2, '0');
-                                const blockedSlotEndMinute = String(Math.floor(blockedSlot.endInMinutes % 60).padStart(2, '0');
+                                const blockedSlotEndMinute = String(Math.floor(blockedSlot.endInMinutes % 60)).padStart(2, '0');
                                 
                                 const isUserBlocked = blockedSlot.isBlocked === true; 
 
